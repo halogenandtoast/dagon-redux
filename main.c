50e255cbd469431b501190f3a9d422ce84674345
Matthew Mongeau <halogenandtoast@gmail.com>
2016-12-10 20:10:12 +0900

    Allow Reading of File

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
+#include <stdio.h>
+
+extern FILE* yyin;
 extern int yylex();
 
-int main() {
+int main(int argc, char* argv[]) {
+  if(argc > 1) {
+    yyin = fopen(argv[1], "r");
+  }
+
   yylex();
   return 0;
 }
